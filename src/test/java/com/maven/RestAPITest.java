e0a790f2fd960c3184e9f2041dcd0216ee8284de
mrinmayee <mrinsurya@gmail.com>
2022-05-25 21:47:55 +0530

    Commit from Asian Timezone

--- src/test/java/com/maven/RestAPITest.java
+++ src/test/java/com/maven/RestAPITest.java
@@ -8,6 +8,11 @@ public class RestAPITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
+		
+		System.out.println("postJira2");
+		
+		System.out.println("postJira3");
+		
 	}
 	
 	@Test
